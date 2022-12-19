7603a52f3c1b1d7052631fdf66845dafd97a6b5e
nexus <116381410+n-40@users.noreply.github.com>
2022-12-18 21:42:50 -0800

    Add files via upload

---
+++ Script-Main/random shit idk.lua
@@ -0,0 +1,27 @@
+--[[
+    im not a skid
+    if i do "borrow" some code i give credits 😎
+    credits ⬇
+]]--
+
+local credits = {
+    "Vape",
+    "Infinite Yeild",
+    "Roblox Developer Forums",
+    "Stack Overflow",
+}
+local EpicPeople = {
+    "nexus#6613",
+    "xylex#8541",
+    "Bolts#0027",
+    "GavinStrikes#0001",
+}
+local you = game:GetService("Players").LocalPlayer
+
+--random splitter lol
+
+if table.find(EpicPeople, you.Name) then
+    print("You are an epic person!")
+else
+    print("ooh... yikes... not an epic person...")
+end
